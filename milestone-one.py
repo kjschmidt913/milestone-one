dc627493b5810efddab253da34eace779afc678b
KJ <kj.schmidt913@gmail.com>
2019-01-27 13:33:21 -0500

    made a print board function using a dictionary of lists. Going to try a list of lists next

--- milestone-one.py
+++ milestone-one.py
@@ -1,1 +1,28 @@
-test
+print("Hello World")
+
+board = {
+    "row1" : [" ","|"," ","|"," "],
+    "border" : ["-","-","-","-","-"],
+    "row2": [" ","|"," ","|"," "],
+    "row3" : [" ","|"," ","|"," "]
+}
+
+def printBoard():
+    for spot in board["row1"]:
+        print(spot, end="")
+    print(" ")
+    for spot in board["border"]:
+        print(spot, end="")
+    print(" ")
+    for spot in board["row2"]:
+        print(spot, end="")
+    print(" ")
+    for spot in board["border"]:
+        print(spot, end="")
+    print(" ")
+    for spot in board["row3"]:
+        print(spot, end="")
+    print("\n")
+
+printBoard()
+    
