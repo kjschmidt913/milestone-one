a02e6c599ce3ae9cc09e72567860299d2391da53
KJ <kj.schmidt913@gmail.com>
2019-01-27 13:37:57 -0500

    deleted commented out old  printboard func and board var

--- milestone-one.py
+++ milestone-one.py
@@ -1,28 +1,18 @@
-print("Hello World")
-
-board = {
-    "row1" : [" ","|"," ","|"," "],
-    "border" : ["-","-","-","-","-"],
-    "row2": [" ","|"," ","|"," "],
-    "row3" : [" ","|"," ","|"," "]
-}
+board = [
+    [" ","|"," ","|"," "],
+    ["-","-","-","-","-"],
+    [" ","|"," ","|"," "],
+    ["-","-","-","-","-"],
+    [" ","|"," ","|"," "]
+]
 
 def printBoard():
-    for spot in board["row1"]:
-        print(spot, end="")
-    print(" ")
-    for spot in board["border"]:
-        print(spot, end="")
-    print(" ")
-    for spot in board["row2"]:
-        print(spot, end="")
-    print(" ")
-    for spot in board["border"]:
-        print(spot, end="")
-    print(" ")
-    for spot in board["row3"]:
-        print(spot, end="")
-    print("\n")
+    for row in board:
+        for spot in row:
+            print(spot, end="") 
+        print(" ")
+    print("\n")   
+
 
 printBoard()
     
