8f224262e49f6f0663a8f04b5bc6d31106d369d9
KJ <kj.schmidt913@gmail.com>
2019-01-27 13:47:27 -0500

    added a while loop to get input in case an invalid marker is chosen

--- milestone-one.py
+++ milestone-one.py
@@ -7,19 +7,26 @@ board = [
 ]
 
 def printBoard():
+    print("\n")
     for row in board:
         for spot in row:
             print(spot, end="") 
         print(" ")
     print("\n") 
 
-playerOne = input("Player One, choose either X or O for your marker: ")
+playerOne = " "
 
-if playerOne == "X":
-    playerTwo = "O"
-else:
-    playerTwo = "X"
-print(f"Player Two, your marker is {playerTwo}")
+while(playerOne != "X") and (playerOne != "O"):
+    playerOne = input("Player One, choose either X or O for your marker: ")
+    if playerOne == "X":
+        playerTwo = "O"
+    elif playerOne == "O":
+        playerTwo = "X"
+    else:
+        print("Not a valid marker. Please choose again.")
+        continue
+    
+    print(f"Player Two, your marker is {playerTwo}")
 
 
 printBoard()
