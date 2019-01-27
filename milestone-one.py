f19b50be04c34cd8e38acc56433acd81b446ccf0
KJ <kj.schmidt913@gmail.com>
2019-01-27 13:41:37 -0500

    asks players for marker choices and saves choice

--- milestone-one.py
+++ milestone-one.py
@@ -11,7 +11,15 @@ def printBoard():
         for spot in row:
             print(spot, end="") 
         print(" ")
-    print("\n")   
+    print("\n") 
+
+playerOne = input("Player One, choose either X or O for your marker: ")
+
+if playerOne == "X":
+    playerTwo = "O"
+else:
+    playerTwo = "X"
+print(f"Player Two, your marker is {playerTwo}")
 
 
 printBoard()
